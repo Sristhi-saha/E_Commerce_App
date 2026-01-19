ac740f639cc162cae20af377417920d4761e92ef
Sristhi-saha <shyamalsaha558@gmail.com>
2026-01-19 21:26:22 +0530

    error handle

--- server/controllers/admin/products-controllers.js
+++ server/controllers/admin/products-controllers.js
@@ -5,7 +5,12 @@ const handleImageUpload = async (req, res) => {
     try {
         const b64 = Buffer.from(req.file.buffer).toString("base64");
         const url = "data:" + req.file.mimetype + ";base64," + b64;
-        const result = await imageUploadUtil(url)
+        const result = await imageUploadUtil(url);
+
+        res.json({
+            success:true,
+            result
+        })
 
     } catch (error) {
         console.log(error);
